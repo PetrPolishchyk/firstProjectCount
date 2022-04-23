b546ccb6af9856ce65813430b3fda8f69a004473
PolishchukPeter <Polishchyk87const@gmail.com>
2022-04-23 16:22:31 +0300

    More wide phrase about world

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world, I am back!");
+       // System.out.println("Hello world, I am back!");
+        System.out.print("Hello World");
+        System.out.println(", how are you?");
+        System.out.println("Do you like mountains?");
     }
 }
