a49aad7dea55e64900c1e09f10e4925bc99f4d82
PolishchukPeter <Polishchyk87const@gmail.com>
2022-04-25 18:54:59 +0300

    New branch with variabless

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,22 @@
 package org.example;
+import java.lang.String;
+import java.lang.System;
 
 public class Main {
-    public static void main(String[] args) {
+    public static <string> void main(String[] args) {
        // System.out.println("Hello world, I am back!");
         System.out.print("Hello World");
         System.out.println(", how are you?");
         System.out.println("Do you like mountains?");
+        byte num = 12;
+        int number = 2985574;
+        float f = 12.88f;
+        boolean trust = true;
+        char loop = 'H';
+        String button = "Hello again!";
+
+        System.out.println(num);
+        System.out.println(number);
+        System.out.println(button);
     }
 }
