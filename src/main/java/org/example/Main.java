138b6d53b9f4098aa07e6ae5e116d76b80cff4a0
PolishchukPeter <Polishchyk87const@gmail.com>
2022-04-26 14:20:48 +0300

    lesson 7 and 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static <string> void main(String[] args) {
        // System.out.println("Hello world, I am back!");
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+ /*
         System.out.println(", how are you?");
         System.out.println("Do you like mountains?");
         byte num = 12;
@@ -16,13 +17,14 @@ public class Main {
         char loop = 'H';
         String button = "Hello again!";
 
-        //System.out.println(num);
-        //System.out.println(number);
-        //System.out.println(button);
+        System.out.println(num);
+        System.out.println(number);
+        System.out.println(button);*/
 
         //Scanner str = new Scanner(System.in);
         //System.out.println("Your string is:" + str.nextLine());
 
+/*
         Scanner numb = new Scanner(System.in);
         int first, second;
         float result;
@@ -32,7 +34,76 @@ public class Main {
         second = numb.nextInt();
         result = first + second;
         result /= 2;
-        System.out.println("Your summ is : " + result);
-        
+        System.out.println("Your result is : " + result);*/
+/*
+        //if constructor
+        Scanner operator = new Scanner(System.in);
+        int oneNum, twoNum;
+        System.out.println("Enter first number:");
+        oneNum = operator.nextInt();
+        System.out.println("Enter second number:");
+        twoNum = operator.nextInt();
+        if (oneNum > twoNum) {
+            System.out.println("First number is bigger than second number");
+        } else if (oneNum < twoNum) {
+            System.out.println("First number is lower than second number");
+        } else System.out.println("First number is equal with second number");
+
+        //System.out.print("\n");
+
+        switch (oneNum) {
+            case  1000:
+                System.out.println("Your first number is 1000");
+                break;
+            case  2000:
+                System.out.println("Your first number is 2000");
+                break;
+            case  3000:
+                System.out.println("Your first number is 3000");
+                break;
+            default:
+                System.out.println("Your first number is not in 1000, 2000, 3000");
+        }
+ */
+ /*
+        //for circle
+        Scanner book = new Scanner(System.in);
+        int m, n;
+        System.out.println("Enter the first number of circle");
+        m = book.nextInt();
+        System.out.println("Enter the second number of circle");
+        n = book.nextInt();
+        for (int i = m, k = n; i <= k; i++) {
+            if (i < k) {
+            System.out.print(i + ",");
+        } else System.out.print(i + ".");
+        }
+        System.out.print("\n");
+        //while circle
+        int j = 10;
+        while (j <= 20) {
+            if (j < 20) {
+            System.out.print(j + ",");
+        } else System.out.print(j + ".");
+            j += 2;
     }
+        System.out.print("\n");
+        //do while
+        boolean isTrue = true;
+        do {
+            System.out.println(isTrue);
+        } while (isTrue = false);
+
+        for (int x = 0; x <= 30; x +=5){
+            if (x < 25) {
+                System.out.println(x);
+            } else {
+                System.out.println("x can't to be more than 20");
+                break;
+            }
+        }
+*/
+        //Array
+
+}
 }
