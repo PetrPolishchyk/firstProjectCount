9af4808998a63633d980578fee891c933242bc9b
PolishchukPeter <Polishchyk87const@gmail.com>
2022-04-28 20:16:21 +0300

    add classes

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Person {
+    public int height = 180;
+    public int weight = 80;
+    public void sayName (String name){
+        System.out.println("Hello, " + name);
+    }
+}
