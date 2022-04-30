4b330156f934f057b249b174fd5c18b7fdeb911d
PolishchukPeter <Polishchyk87const@gmail.com>
2022-04-30 19:56:15 +0300

    constructor and nasl

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,16 +1,18 @@
 package org.example;
 
 public class Student extends Person {
-    int course;
+    int course = 1;
 
     public Student (int height, String phrase, int course){
         super(height, phrase);
         this.course = course;
     }
+    public  Student (){}
 
     void tell (){
         System.out.println(super.phrase);
         System.out.println(super.height);
         System.out.println(course);
+        super.sayName("Experiment");
     }
 }
